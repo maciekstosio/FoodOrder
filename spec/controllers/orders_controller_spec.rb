3e63897950aa0d0f16c891eb800d3267d78f6de5
Maciek <maciekstosio@icloud.com>
2017-06-20 01:16:39 +0200

    Add some orders controller test

---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,79 @@
+require "rails_helper"
+
+RSpec.describe OrdersController do
+  before(:all) do
+    # Add users
+    @user = User.create!(nickname: "Tester", email: "tester@domain.com")
+    user = User.create!(nickname: "User", email: "user@domain.com")
+    user2 = User.create!(nickname: "User2", email: "user2@domain.com")
+
+    # Add lists
+    list = user.lists.create!(name: "List1")
+    list2 = user2.lists.create!(name: "List2")
+
+    #Add orders
+    order = list.orders.create!(name: "Order", price: 10, user_id: user.id)
+  end
+
+  describe "POST #create" do
+    it "responds HTTP 401 to unauthorized user" do
+      post :create, params: { order: { name: "Test", price: 10 }}
+
+      expect(response).to have_http_status(401)
+    end
+
+    it "responds HTTP 404 to authorized user when list doesn't exist" do
+      request.headers.merge!(@user.create_new_auth_token)
+      post :create, params: { order: { list_id: 666, name: "Test", price: 10 }}
+
+      expect(response). to have_http_status(404)
+    end
+
+    it "responds HTTP 403 to authorized user when list is closed" do
+      list = @user.lists.create!(name: "Test", state: 3)
+
+      request.headers.merge!(@user.create_new_auth_token)
+      post :create, params: { order: { list_id: list.id, name: "Test", price: 10 }}
+
+      expect(response).to have_http_status(403)
+    end
+
+    it "responds HTTP 400 to authorized user when name doesn't exist" do
+      list = @user.lists.create!(name: "Test")
+
+      request.headers.merge!(@user.create_new_auth_token)
+      post :create, params: { order: { list_id: list.id, price: 10 }}
+
+      expect(response).to have_http_status(400)
+    end
+
+    it "responds HTTP 400 to authorized user when name is to short" do
+      list = @user.lists.create!(name: "Test")
+
+      request.headers.merge!(@user.create_new_auth_token)
+      post :create, params: { order: { list_id: list.id, name: "as", price: 10 }}
+
+      expect(response).to have_http_status(400)
+    end
+
+    it "responds HTTP 400 to authorized user when price doesn't exist" do
+      list = @user.lists.create!(name: "Test")
+
+      request.headers.merge!(@user.create_new_auth_token)
+      post :create, params: { order: { list_id: list.id, name: "Test" }}
+
+      expect(response).to have_http_status(400)
+    end
+
+    it "responds HTTP 400 to authorized user when price is less than 0" do
+      list = @user.lists.create!(name: "Test")
+
+      request.headers.merge!(@user.create_new_auth_token)
+      post :create, params: { order: { list_id: list.id, name: "Test", price: -1 }}
+
+      expect(response).to have_http_status(400)
+    end
+    
+    #Precision test
+  end
+end
