d0154ffd1af46de4b84c0aca2869843bdcd91497
Maciek <maciekstosio@icloud.com>
2017-06-20 01:36:40 +0200

    Add checking for List ID presence

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,35 +2,41 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    order = current_user.orders.build(order_params)
-    list = List.where(id: params[:order][:list_id])
-    if list.present?
-      if list.first.state==0
-        if current_user.orders.where(list_id: params[:list_id]).count==0
-          if order.save
-            render json: {
-              messages: ["Order added successfuly"],
-              id: order.id
-            }, status: 200
+    if !params[:order][:list_id].nil?
+      order = current_user.orders.build(order_params)
+      list = List.where(id: params[:order][:list_id])
+      if list.present?
+        if list.first.state==0
+          if current_user.orders.where(list_id: params[:list_id]).count==0
+            if order.save
+              render json: {
+                messages: ["Order added successfuly"],
+                id: order.id
+              }, status: 200
+            else
+              render json: {
+                messages: order.errors.full_messages
+              }, status: 400
+            end
           else
             render json: {
-              messages: order.errors.full_messages
-            }, status: 400
+              messages: ["You can't order more than one thing"]
+            }, status: 403
           end
         else
           render json: {
-            messages: ["You can't order more than one thing"]
+            messages: ["This list is closed, you can't add new order"]
           }, status: 403
         end
       else
         render json: {
-          messages: ["This list is closed, you can't add new order"]
-        }, status: 403
+          messages: ["List doesn't exists"]
+        }, status: 404
       end
     else
       render json: {
-        messages: ["List doesn't exists"]
-      }, status: 404
+        messages: ["Id of list must be given"]
+      }, status: 400
     end
   end
 
