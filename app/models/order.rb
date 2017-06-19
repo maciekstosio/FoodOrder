4fe101c085631e94c2002b7ab756158cb24762d4
Maciek <maciekstosio@icloud.com>
2017-06-20 01:22:50 +0200

    Fix order's price range validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,5 @@ class Order < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { minimum: 3 }
-  validates :price, presence: true, numericality: { grather_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 0, less_than: 10000 }
 end
