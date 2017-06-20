e673675e7aae7fe67389b2420ad98689991438f4
Maciek <maciekstosio@icloud.com>
2017-06-21 00:56:43 +0200

    Make lists controller readable

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,71 +4,57 @@ class ListsController < ApplicationController
   def index
     lists = List.order(id: :desc).all
     orders = Order.joins(:user).select('orders.*, users.name as username, users.nickname as usernickname, users.image as userimage').all
-    render json: { lists: lists, orders: orders} , status: 200
+    long_resp({ lists: lists, orders: orders} , 200)
   end
 
   def create
     list = current_user.lists.build(list_params)
     if list.save
-      render json: {
-        messages: ["List created successfuly"],
-        id: list.id
-      }, status: 200
+      long_resp({ messages: ["List created successfuly"], id: list.id }, 200)
     else
-      render json: {
-        messages: list.errors.full_messages
-      }, status: 400
+      long_resp({ messages: list.errors.full_messages }, 400)
     end
   end
 
   def update
     list = List.where(id: params[:id])
-    if list.present?
-      if list.first.user_id==current_user.id
-        update_list = list.first
-        if update_list.update_attributes(state_params)
-          render json: {
-            messages: ["State changed successfuly"]
-          }, status: 200
-        else
-          render json: {
-            messages: update_list.errors.full_messages
-          }, status: 400
-        end
-      else
-        render json: {
-          messages: ["You don't have permission to change status of this list"]
-        }, status: 403
-      end
+
+    if list.blank?
+      resp "List doesn't exit", 404
+      return
+    end
+
+    if list.first.user_id!=current_user.id
+      resp "You don't have permission to change status of this list", 403
+      return
+    end
+
+    update_list = list.first
+
+    if update_list.update_attributes(state_params)
+      resp "State changed successfuly", 200
     else
-      render json: {
-        messages: ["List doesn't exist"]
-      }, status: 404
+      long_resp({ messages: update_list.errors.full_messages }, 400)
     end
   end
 
   def destroy
     list = List.where(id: params[:id])
-    if list.present?
-      if list.first.user_id==current_user.id
-        if list.first.destroy
-          render json: {
-            messages: ["List deleted successfuly"]
-          }, status: 200
-        else
-          render json: {
-            messages: errors.full_messages
-          }, status: 400
-        end
-      else
-        render json: {
-          messages: ["You don't have permission to change status of this list"]
-        }, status: 403
-      end
+
+    if list.blank?
+      resp "List doesn't exist", 404
+      return
+    end
+
+    if list.first.user_id!=current_user.id
+      resp "You don't have permission to change state of this list", 403
+      return
+    end
+
+    if list.first.destroy
+      resp "List deleted successfuly", 200
     else
-      render json: {
-        messages: ["List doesn't exist"]
-      }, status: 404
+      long_resp({ messages: errors.full_messages }, status: 400)
     end
   end
 
@@ -80,5 +66,15 @@ class ListsController < ApplicationController
 
   def state_params
     params.require(:list).permit(:state)
+  end
+
+  def resp(text,code)
+    render json: {
+      messages: [text]
+    }, status: code
+  end
+
+  def long_resp(hash,code)
+    render json: hash, status: code
   end
 end
