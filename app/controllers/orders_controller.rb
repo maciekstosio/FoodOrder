8549111eef037e9a4451d543a14c7da3cb49d80a
Maciek <maciekstosio@icloud.com>
2017-06-20 23:37:22 +0200

    Make orders controller readable

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,79 +2,66 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    if !params[:order][:list_id].nil?
-      order = current_user.orders.build(order_params)
-      list = List.where(id: params[:order][:list_id])
-      if list.present?
-        if list.first.state==0
-          if current_user.orders.where(list_id: params[:order][:list_id]).count==0
-            if order.save
-              render json: {
-                messages: ["Order added successfuly"],
-                id: order.id
-              }, status: 200
-            else
-              render json: {
-                messages: order.errors.full_messages
-              }, status: 400
-            end
-          else
-            render json: {
-              messages: ["You can't order more than one thing"]
-            }, status: 403
-          end
-        else
-          render json: {
-            messages: ["This list is closed, you can't add new order"]
-          }, status: 403
-        end
-      else
-        render json: {
-          messages: ["List doesn't exists"]
-        }, status: 404
-      end
+    if params[:order][:list_id].nil?
+      resp "ID must be given", 400
+      return
+    end
+
+    list = List.where(id: params[:order][:list_id])
+
+    if list.blank?
+      resp "List doesn't exist", 404
+      return
+    end
+
+    if list.first.state!=0
+      resp "This list is closed, you can't add new order", 403
+      return
+    end
+
+    if current_user.orders.where(list_id: params[:order][:list_id]).count!=0
+      resp "You can't order more than one think", 403
+      return
+    end
+
+    order = current_user.orders.build(order_params)
+
+    if order.save
+      long_resp({ messages: ["Order added successfuly"], id: order.id }, 200)
     else
-      render json: {
-        messages: ["Id of list must be given"]
-      }, status: 400
+      long_resp({ messages: order.errors.full_messages }, 400)
     end
   end
 
   def destroy
     list = List.where(id: params[:list_id])
+
+    if list.blank?
+      resp "List doesn't exsist", 404
+      return
+    end
+
     order = Order.where(id: params[:id], list_id: params[:list_id])
-    if list.present?
-      if order.present?
-        if order.first.user_id == current_user.id
-          if list.first.state==0
-            if order.first.destroy
-              render json: {
-                messages: ["Order deleted successfuly"]
-              }, status: 200
-            else
-              render json: {
-                messages: order.errors.full_messages
-              }, status: 400
-            end
-          else
-            render json: {
-              messages: ["This list is closed, you can't delete your order"]
-            }, status: 403
-          end
-        else
-          render json: {
-            messages: ["You don't have permission to delete this order"]
-          }, status: 403
-        end
-      else
-        render json: {
-          messages: ["Order doesn't exists"]
-        }, status: 404
-      end
+
+    if order.blank?
+      resp "Order doesn't exist", 404
+      return
+    end
+
+    if order.first.user_id != current_user.id
+      resp "You don't have permission to delete this order", 403
+      return
+    end
+
+    if list.first.state!=0
+      resp "This list is closed, you can't delete your order", 403
+      return
+    end
+
+    if order.first.destroy
+      resp "Order deleted successfuly", 200
     else
-      render json: {
-        messages: ["List doesn't exists"]
-      }, status: 404
+      long_resp({ messages: order.errors.full_messages }, 400)
     end
   end
 
@@ -82,5 +69,15 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order).permit(:name,:price,:list_id);
+  end
+
+  def resp(text,code)
+    render json: {
+      messages: [text]
+    }, status: code
+  end
+
+  def long_resp(hash,code)
+    render json: hash, status: code
   end
 end
